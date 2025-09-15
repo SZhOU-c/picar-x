df9dc8a885e75ead9562d4878c68985f562a07e4
SZhOU-c <joeqvq@gmail.com>
2025-09-14 20:48:09 -0500

    Update servo_zeroing.py

--- example/servo_zeroing.py
+++ example/servo_zeroing.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     print(f"Set servo to zero")
     for i in range(12):
         # print(f"Servo {i} set to zero")
-        Servo(i).angle(0)
-        sleep(0.1)
         Servo(i).angle(10)
+        sleep(0.1)
+        Servo(i).angle(0)
         sleep(0.1)
     while True:
         sleep(1)
