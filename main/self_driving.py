24d0c0a6f5d81af44bce4cfc2fe0e32d648e8e02
SZhOU-c <joeqvq@gmail.com>
2025-09-15 23:38:09 -0500

    Update self_driving.py

--- main/self_driving.py
+++ main/self_driving.py
@@ -16,15 +16,15 @@ def avoid_obstacle(px, distance):
 
         px.backward(POWER)
         backup_time = 1 / max(distance , 4) # backup_time = [0.025, 0.25]
-        turn_angle = random.choice([15, 30])
-        turn_direction = random.choice((-1, 1)) * turn_angle
-        px.set_dir_servo_angle(turn_direction)
+        turn_angle = random.choice([-30, 30])
+        #turn_direction = random.choice((-1, 1)) * turn_angle
+        px.set_dir_servo_angle(turn_angle)
         time.sleep(backup_time)  # back up a bit longer
     else:
         print("Obstacle ahead, turning...")
-        turn_angle = random.choice([15, 30])
-        turn_direction = random.choice((-1, 1)) * turn_angle
-        px.set_dir_servo_angle(turn_direction)
+        turn_angle = random.choice([-30, 30])
+        #turn_direction = random.choice((-1, 1)) * turn_angle
+        px.set_dir_servo_angle(turn_angle)
         px.forward(POWER)
         time.sleep(0.1)
 
