58a6e91714b1e8b0bc4fcc6c93d33a42d367a38e
SZhOU-c <joeqvq@gmail.com>
2025-09-23 17:56:31 -0500

    Update calibrate_speed_ultrasonic.py

--- main/calibrate_speed_ultrasonic.py
+++ main/calibrate_speed_ultrasonic.py
@@ -5,15 +5,33 @@ import time
 SafeDistance = 40   # > 40 safe
 DangerDistance = 20 # > 20 && < 40 turn around, 
                     # < 20 backward
+px = Picarx()
 
 def main():
     try:
         power = 50
-        px = Picarx()
+        
         px.set_dir_servo_angle(0)
         px.set_cam_pan_angle(0)
         # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
         px.ultrasonic.read()
+
+        start = px.ultrasonic.read()
+        px.forward(power)
+
+        time.sleep(0.5)
+        start = px.ultrasonic.read()
+        time.sleep(0.5)
+        end = px.ultrasonic.read()
+        px.forward(0)
+        distance = start - end
+        print("distance",distance , " velocity = ", distance/0.5)
+
+    finally:
+        px.forward(0)
+
+def speed_test():
+        power = 50
         distance = [0.0,1.1,2.1,3.1]
         distance[0] = round(px.ultrasonic.read(), 2)
         print("initial distance: ",distance[0])
@@ -68,12 +86,6 @@ def main():
         distance[3] = round(px.ultrasonic.read(), 2)
         moved = distance[2] - distance[3]
         print("after 0.5 second moved ",moved , " velocity = ", moved/0.5)
-
-
-    finally:
-        px.forward(0)
-
-
 
 
 if __name__ == "__main__":
