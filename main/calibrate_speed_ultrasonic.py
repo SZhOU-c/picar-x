2f33bf40fd705da863ee0eb52b061526e2112723
SZhOU-c <joeqvq@gmail.com>
2025-09-23 17:33:20 -0500

    Create calibrate_speed_ultrasonic.py

---
+++ main/calibrate_speed_ultrasonic.py
@@ -0,0 +1,79 @@
+from picarx import Picarx
+import time
+
+
+SafeDistance = 40   # > 40 safe
+DangerDistance = 20 # > 20 && < 40 turn around, 
+                    # < 20 backward
+
+def main():
+    try:
+        power = 50
+        px = Picarx()
+        # px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
+        distance = []
+        distance[0] = round(px.ultrasonic.read(), 2)
+        print("initial distance: ",distance[0])
+
+        px.set_dir_servo_angle(0)
+        px.forward(power)
+        time.sleep(0.1)
+        px.forward(0)
+        distance[1] = round(px.ultrasonic.read(), 2)
+        moved = distance[0] - distance[1]
+        print("after 0.1 second moved ",moved , " velocity = ", moved/0.1)
+
+
+        px.forward(power)
+        time.sleep(0.2)
+        px.forward(0)
+        distance[2] = round(px.ultrasonic.read(), 2)
+        moved = distance[1] - distance[2]
+        print("after 0.2 second moved ",moved , " velocity = ", moved/0.2)
+
+        px.set_dir_servo_angle(0)
+        px.forward(power)
+        time.sleep(0.5)
+        px.forward(0)
+        distance[3] = round(px.ultrasonic.read(), 2)
+        moved = distance[2] - distance[3]
+        print("after 0.5 second moved ",moved , " velocity = ", moved/0.5)
+
+
+        power = 30
+        distance[0] = round(px.ultrasonic.read(), 2)
+        print("Now power = 30, initial distance: ",distance[0])
+
+        px.forward(power)
+        time.sleep(0.1)
+        px.forward(0)
+        distance[1] = round(px.ultrasonic.read(), 2)
+        moved = distance[0] - distance[1]
+        print("after 0.1 second moved ",moved , " velocity = ", moved/0.1)
+
+
+        px.forward(power)
+        time.sleep(0.2)
+        px.forward(0)
+        distance[2] = round(px.ultrasonic.read(), 2)
+        moved = distance[1] - distance[2]
+        print("after 0.2 second moved ",moved , " velocity = ", moved/0.2)
+
+        px.set_dir_servo_angle(0)
+        px.forward(power)
+        time.sleep(0.5)
+        px.forward(0)
+        distance[3] = round(px.ultrasonic.read(), 2)
+        moved = distance[2] - distance[3]
+        print("after 0.5 second moved ",moved , " velocity = ", moved/0.5)
+
+
+    finally:
+        px.forward(0)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
